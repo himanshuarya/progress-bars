fdb5b7ac0b096128472cf023d6a9233de81a77ad
Himanshu Arya <Himanshu_Arya@external.mckinsey.com>
2018-01-11 14:26:21 +0530

    Adding the functionality of creating progress bars

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,35 @@
 (function(){
-   'use strict'; 
+    'use strict';
+   
+    var client = new HttpClient();
+    client.get('http://pb-api.herokuapp.com/bars', function(responseText) {
+        var response = JSON.parse(responseText);       
+        createProgressBars(response.bars, response.limit);       
+    });
+    
+    function createProgressBars(bars, limit){
+        var progressBars = document.getElementById('progressBars');
+        
+        bars.forEach(function (value, index){
+            var percentValue = (value/limit)*100;
+            
+            var progressBar = document.createElement('div');
+            progressBar.id = 'progressBar' + (index + 1);
+            progressBar.className = 'progress-bar';
+
+            var bar = document.createElement('div');
+            bar.className = 'bar';
+            bar.style.width = percentValue + '%';
+
+            var label = document.createElement('div');
+            label.className = 'progress-bar-label';
+            label.innerHTML = value + '/' + limit + ' (' + percentValue.toFixed(0) + '%' + ')';
+
+            progressBar.appendChild(label);
+            progressBar.appendChild(bar);            
+            progressBars.appendChild(progressBar);            
+        });  
+    };
     
 })();
 
---
+++ app/js/httpService.js
@@ -0,0 +1,13 @@
+var HttpClient = function() {
+    this.get = function(url, callback) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() { 
+            if (httpRequest.readyState === 4 && httpRequest.status === 200){
+                callback(httpRequest.responseText);
+            }
+        };
+        httpRequest.open( "GET", url, true );            
+        httpRequest.send( null );
+    };
+};
+
