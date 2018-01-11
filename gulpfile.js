956b0b0ecc527d07cf8d2c75c9504037dd48a0bd
Himanshu Arya <Himanshu_Arya@external.mckinsey.com>
2018-01-11 12:16:06 +0530

    Adding the basic project structure, package.json and integrating Gulp build

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+    sass = require('gulp-sass');
+    browserSync = require('browser-sync').create();
+    useref = require('gulp-useref');
+    uglify = require('gulp-uglify');
+    gulpIf = require('gulp-if');
+    cssnano = require('gulp-cssnano');
+    del = require('del');
+    runSequence = require('run-sequence');
+    jshint = require('gulp-jshint');
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('lint', function() {
+  return gulp.src('app/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+    
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  });
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', 'sass', 'lint', 'useref', 
+    callback
+  );
+});
+
+gulp.task('default', function (callback) {
+  runSequence(['sass', 'browserSync', 'watch'],
+    callback
+  );
+});
