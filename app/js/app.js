9ac46fee11e873da2ad26ef5547a295fb643038d
Himanshu Arya <Himanshu_Arya@external.mckinsey.com>
2018-01-11 15:26:16 +0530

    Adding control elements for the progress bars

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,11 @@
 (function(){
     'use strict';
-   
-    var client = new HttpClient();
-    client.get('http://pb-api.herokuapp.com/bars', function(responseText) {
-        var response = JSON.parse(responseText);       
-        createProgressBars(response.bars, response.limit);       
-    });
     
     function createProgressBars(bars, limit){
         var progressBars = document.getElementById('progressBars');
         
         bars.forEach(function (value, index){
-            var percentValue = (value/limit)*100;
-            
+            var percentValue = (value/limit)*100;            
             var progressBar = document.createElement('div');
             progressBar.id = 'progressBar' + (index + 1);
             progressBar.className = 'progress-bar';
@@ -30,6 +23,38 @@
             progressBars.appendChild(progressBar);            
         });  
     };
+    
+    function createDropdown(length){
+        var controls = document.getElementById('controls');
+        var dropdown = document.createElement('select');
+        dropdown.className = 'dropdown';
+        var container = document.getElementById('container');
+        container.appendChild(dropdown);
+        for(var i=1; i <= length; i++){
+            var item = document.createElement('option');
+            item.value = 'progressBar' + i;
+            item.innerHTML = 'Progress Bar ' + i; 
+            dropdown.appendChild(item);
+        }
+        controls.appendChild(dropdown);
+    };
+    
+    function createButtons(buttons){
+        var controls = document.getElementById('controls');
+        buttons.forEach(function (value){
+            var button = document.createElement('button');
+            button.innerHTML = value > 0 ? ('+' + value) : ('' + value);
+            controls.appendChild(button);
+        });       
+    };
+   
+    var client = new HttpClient();
+    client.get('http://pb-api.herokuapp.com/bars', function(responseText) {
+        var response = JSON.parse(responseText);       
+        createProgressBars(response.bars, response.limit);
+        createDropdown(response.bars.length);
+        createButtons(response.buttons);
+    });   
     
 })();
 
